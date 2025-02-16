aad4bf86c3ad7c037cdaaebfc4cd43074c37d397
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2025-02-16 21:50:19 +0100

    Create Entry.cs

---
+++ Game-Hacking/AssaultCube/16.2.25/Entry.cs
@@ -0,0 +1,9 @@
+public static class Entry{
+	
+	public static void Main(){
+		Console.WriteLine("Starting Program...");
+		Console.WriteLine("This is made for Educational Purposes only!");
+		Proc _process = new Proc();
+		_process.GetProcess();
+	}
+}
