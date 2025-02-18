1fe03d4d2f45cd483ab67609e36f6da099316900
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2025-02-18 14:59:06 +0100

    Update Entry.cs

--- Game-Hacking/AssaultCube/16.2.25/Entry.cs
+++ Game-Hacking/AssaultCube/16.2.25/Entry.cs
@@ -1,7 +1,6 @@
 public static class Entry{
 	
 	public static void Main(){
-		Console.WriteLine("Starting Program...");
 		Console.WriteLine("This is made for Educational Purposes only!");
 		Proc _process = new Proc();
 		_process.GetProcessInfo();
