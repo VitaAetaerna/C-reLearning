271dd7fa293035e618eafc4c9bc6fdf5d2791588
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2025-02-18 14:55:31 +0100

    Add files via upload

---
+++ Game-Hacking/AssaultCube/16.2.25/values.cs
@@ -0,0 +1,7 @@
+public static class Values{
+	public static IntPtr Health = "";
+	public static IntPtr Ammo = "";
+	public static IntPtr Speed = "";
+	public static IntPtr Armor = "";
+	public static IntPtr Grenades = "";
+}
