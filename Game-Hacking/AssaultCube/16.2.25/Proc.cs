a2dcaa246447529f5ea6778c9e5ea0d1986e3ad9
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2025-02-18 14:56:11 +0100

    Update Proc.cs

--- Game-Hacking/AssaultCube/16.2.25/Proc.cs
+++ Game-Hacking/AssaultCube/16.2.25/Proc.cs
@@ -1,14 +1,17 @@
 using System.Diagnostics;
 
 public class Proc{
+	private ProcModify _procModify = new ProcModify(); // Used for the Memory Manipulation
 	private string _processName = "Spotify"; // Process Name 
+
+	// Preperation Phase - Get all the basic stuff like Handle, Process ID and so on
 	public void GetProcessInfo() {
 		var mainProc = GetMainProcess(_processName);
 		var procId = mainProc.Id;
 		var baseHandle = getBaseHandle(mainProc);
 	}
 
-	public  GetMainProcess(string processName){
+	public Process GetMainProcess(string processName){
 		Console.WriteLine("Getting the Process name " + processName);
 		var processListSnapshot = Process.GetProcesses(); // Get All Local Running Processes if needed
 		var processSnapshot = Process.GetProcessesByName(processName); // Get All Currently Running Processes for the Game Specifically
@@ -16,7 +19,7 @@ public class Proc{
 		// If no process of Game was found -> Exit
 		if (processSnapshot == null){
 			Console.WriteLine("Could not find the target's Process Name / Process ID");
-			return; 
+			return null; 
 		}
 		// Extract the Main Process that i want to edit and read memory from
 		var mainProcess = processSnapshot[0];
@@ -29,10 +32,10 @@ public class Proc{
 		return mainProcess;
 	}
 
-	public int getBaseHandle(Process mainProcess){
-		// Get the baseHandle to interact with the Process
-		var baseHandle = mainProcess != null ? mainProcess.Handle : null;
-		if (baseHandle == null){
+	public nint getBaseHandle(Process mainProcess){
+		// Get the base Handle to interact with the Process
+		var baseHandle = mainProcess.Handle;
+		if (baseHandle == 0){
 			Console.WriteLine("Base Handle of Process " + mainProcess + mainProcess.Id + " could not be fetched");
 		} 
 		Console.WriteLine("Parent Process is " + mainProcess + " Base Handle: " + baseHandle);
