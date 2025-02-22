1ed7e35f2fd72e8d21a7d9cbc57dc85ddfc1c820
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2025-02-22 13:27:06 +0100

    Delete Game-Hacking directory

--- Game-Hacking/AssaultCube/16.2.25/Entry.cs
+++
@@ -1,8 +0,0 @@
-public static class Entry{
-	
-	public static void Main(){
-		Console.WriteLine("This is made for Educational Purposes only!");
-		Proc _process = new Proc();
-		_process.GetProcessInfo();
-	}
-}
